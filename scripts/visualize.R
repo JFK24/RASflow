207782069ae8f188cfa46baacb8798d430ccc198
JFK24 <fontaine@genomicscomputbiol.org>
2020-03-17 16:32:23 +0100

    Now restriced to human genes! Add PCA plots. Log2 values for heatmap and pca

--- scripts/visualize.R
+++ scripts/visualize.R
@@ -1,18 +1,36 @@
+# 2020-03-17: log2 values for heatmap and pca
+# 2020-03-16: pca plots and bug fixes (free axis limits of volcano plot)
+#             remove dependency to online resources (ensembldb)
+#             but it is now restricted to human data (EnsDb.Hsapiens.v86)!
+# To do: fix restriction to human data by reading in a gene annotation file
+
 # load the libraries
 if (!require("plotscale")) install.packages('scripts/plotscale_0.1.6.tar.gz', repos = NULL, type="source")
-library(yaml)
-library(hash)
-library(mygene)
-library(EnhancedVolcano)
-library(plotscale)
+library(yaml, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(hash, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(mygene, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(EnhancedVolcano, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(plotscale, warn.conflicts = FALSE, quietly = TRUE, verbose = FALSE)
+library(GenomicFeatures)
+library(EnsDb.Hsapiens.v86)
+library(FactoMineR)
+library(factoextra)
+library(ggpubr)
+library(dplyr)
 
 # ====================== load parameters in config file ======================
 
 # passing the params from command line
 args <- commandArgs(TRUE)
-norm.path <- args[1]
-dea.path <- args[2]
-out.path <- args[3]
+norm.path <- args[1] # norm.path <- 'output/GSE63511/trans/dea/countGroup'
+dea.path <- args[2]  # dea.path <- 'output/GSE63511/trans/dea'
+out.path <- args[3]  # out.path <- 'output/GSE63511/trans/dea/visualization'
+
+#norm.path <- 'output/GSE63511/trans/dea/countGroup'
+#dea.path <- 'output/GSE63511/trans/dea/DEA/gene-level'
+#out.path <- 'output/GSE63511/trans/dea/visualization'
+#name.control <- "Normal"
+#name.treat <- "Tumour"
 
 # load the config file
 if (length(args) > 3) {  # this script is used in  visualize_test.rules
@@ -53,41 +71,51 @@ plot.volcano.heatmap <- function(name.control, name.treat) {
   }
 
   gene.id.dea <- row.names(dea.table)
+  gene.id.dea.symbols <- ensembldb::select(EnsDb.Hsapiens.v86, keys= gene.id.dea, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
+  gene.dea <- merge(data.frame(GENEID=gene.id.dea), gene.id.dea.symbols, all.x=TRUE)$SYMBOL
+  gene.dea <- left_join(data.frame(GENEID=gene.id.dea, stringsAsFactors=FALSE), gene.id.dea.symbols, by = "GENEID")$SYMBOL
 
-  gene.symbol.dea.all <- queryMany(gene.id.dea, scopes = 'ensembl.gene', fields = 'symbol')
+  # gene.symbol.dea.all <- queryMany(gene.id.dea, scopes = 'ensembl.gene', fields = 'symbol')
+  # 
+  # h <- hash()
+  # for (i in 1:nrow(gene.symbol.dea.all)) {
+  #   query <- gene.symbol.dea.all$query[i]
+  #   symbol <- gene.symbol.dea.all$symbol[i]
+  #   if (has.key(query, h)) {  # if there's duplicate for the same query
+  #     h[[query]] <- paste(hash::values(h, keys = query), symbol, sep = ', ')
+  #   } else {
+  #     if (is.na(symbol)) {  # if there's no hit for the query, keep the original id
+  #       h[[query]] <- query
+  #     } else {
+  #       h[[query]] <- symbol
+  #     }
+  #   }
+  # }
+  # 
+  # gene.dea <- gene.id.dea
+  # for (i in c(1:length(gene.dea))) {
+  #   gene.dea[i] <- h[[gene.id.dea[i]]]
+  # }
   
-  h <- hash()
-  for (i in 1:nrow(gene.symbol.dea.all)) {
-    query <- gene.symbol.dea.all$query[i]
-    symbol <- gene.symbol.dea.all$symbol[i]
-    if (has.key(query, h)) {  # if there's duplicate for the same query
-      h[[query]] <- paste(hash::values(h, keys = query), symbol, sep = ', ')
-    } else {
-      if (is.na(symbol)) {  # if there's no hit for the query, keep the original id
-        h[[query]] <- query
-      } else {
-        h[[query]] <- symbol
-      }
-    }
-  }
   
-  gene.dea <- gene.id.dea
-  for (i in c(1:length(gene.dea))) {
-    gene.dea[i] <- h[[gene.id.dea[i]]]
-  }
-
   # volcano plot
+  #-----------------------------------------------------------------------------
   if (dea.tool == 'edgeR') {
+    # fig.volcano <- EnhancedVolcano(dea.table, lab = gene.dea, xlab = bquote(~Log[2]~ "fold change"), x = 'logFC', y = 'FDR', pCutoff = 10e-5, col = c("grey30", "orange2", "royalblue", "red2"),
+    #                                FCcutoff = 1, xlim = c(-5, 5), ylim = c(0, 10), transcriptPointSize = 1.5, title = NULL, subtitle = NULL)  
     fig.volcano <- EnhancedVolcano(dea.table, lab = gene.dea, xlab = bquote(~Log[2]~ "fold change"), x = 'logFC', y = 'FDR', pCutoff = 10e-5, col = c("grey30", "orange2", "royalblue", "red2"),
-                                 FCcutoff = 1, xlim = c(-5, 5), ylim = c(0, 10), transcriptPointSize = 1.5, title = NULL, subtitle = NULL)  
+                                   FCcutoff = 1, transcriptPointSize = 1.5, title = NULL, subtitle = NULL)  
   } else if (dea.tool == 'DESeq2') {
+    # fig.volcano <- EnhancedVolcano(dea.table, lab = gene.dea, xlab = bquote(~Log[2]~ "fold change"), x = 'log2FoldChange', y = 'padj', pCutoff = 10e-5, col = c("grey30", "orange2", "royalblue", "red2"),
+    #                                FCcutoff = 1, xlim = c(-5, 5), ylim = c(0, 10), transcriptPointSize = 1.5, title = NULL, subtitle = NULL)
     fig.volcano <- EnhancedVolcano(dea.table, lab = gene.dea, xlab = bquote(~Log[2]~ "fold change"), x = 'log2FoldChange', y = 'padj', pCutoff = 10e-5, col = c("grey30", "orange2", "royalblue", "red2"),
-                                 FCcutoff = 1, xlim = c(-5, 5), ylim = c(0, 10), transcriptPointSize = 1.5, title = NULL, subtitle = NULL)
+                                   FCcutoff = 1, transcriptPointSize = 1.5, title = NULL, subtitle = NULL)
   }
   
   as.pdf(fig.volcano, width = 9, height = 6, scaled = TRUE, file = file.path(out.path, paste('volcano_plot_', name.control, '_', name.treat, '.pdf', sep = '')))
 
   # heatmap
+  #-----------------------------------------------------------------------------
   norm.table.control <- read.table(norm.control, header = TRUE, row.names = 1)
   norm.table.treat <- read.table(norm.treat, header = TRUE, row.names = 1)
 
@@ -102,8 +130,10 @@ plot.volcano.heatmap <- function(name.control, name.treat) {
   norm.table.deg <- norm.table[index.deg,]
 
   gene.id.norm.table <- rownames(norm.table.deg)
+  gene.id.norm.table.symbols <- ensembldb::select(EnsDb.Hsapiens.v86, keys= gene.id.norm.table, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
+  gene.symbol.norm.table <- merge(data.frame(GENEID=gene.id.norm.table), gene.id.norm.table.symbols, all.x=TRUE)$SYMBOL
 
-  gene.symbol.norm.table <- queryMany(gene.id.norm.table, scopes = 'ensembl.gene', fields = 'symbol')$symbol
+#  gene.symbol.norm.table <- queryMany(gene.id.norm.table, scopes = 'ensembl.gene', fields = 'symbol')$symbol
 
   # if can't find a symbol for the id, then keep the id as it is
   gene.norm.table <- gene.symbol.norm.table
@@ -115,14 +145,57 @@ plot.volcano.heatmap <- function(name.control, name.treat) {
 
   palette <- c("#999999", "#377EB8")
   palette.group <- c(rep(palette[1], num.control), rep(palette[2], num.treat))
+  palette.group.names <- c(rep(name.control, num.control), rep(name.treat, num.treat))
 
   ## draw heatmap
-  pdf(file = file.path(out.path, paste('heatmap_', name.control, '_', name.treat, '.pdf', sep = '')), width = 15, height = 12, title = 'Heatmap using the top features')
-  heatmap(as.matrix(norm.table.deg), ColSideColors = palette.group, margins = c(9,5.5), labRow = gene.norm.table, cexRow = 1.9, cexCol = 1.9)
+  pdf(file = file.path(out.path, paste('heatmap_', name.control, '_', name.treat, '.pdf', sep = '')), width = 15, height = 15, title = 'Heatmap using the top features')
+#  heatmap(as.matrix(norm.table.deg), ColSideColors = palette.group, margins = c(15,15), labRow = gene.norm.table, cexRow = 1.9, cexCol = 1.9)
+  heatmap(as.matrix(log2(norm.table.deg+0.00000001)), ColSideColors = palette.group, margins = c(15,15), labRow = gene.norm.table, cexRow = 1.9, cexCol = 1.9)
   legend("topleft", title = 'Group', legend=groups, text.font = 15,
          col = palette, fill = palette, cex=1.8)
   dev.off()
+
+  # PCA
+  #-----------------------------------------------------------------------------
+#  http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/112-pca-principal-component-analysis-essentials/
+#  pca.table <- norm.table[which(apply(norm.table, 1, var) != 0), ]
+  pca.table <- log2(norm.table[which(apply(norm.table, 1, var) != 0), ]+0.00000001)
+  pca.table.ids <- rownames(pca.table)
+  pca.table.symbols <- ensembldb::select(EnsDb.Hsapiens.v86, keys=pca.table.ids, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
+  pca.table.symbols <- merge(data.frame(GENEID=pca.table.ids), pca.table.symbols, all.x=TRUE)$SYMBOL
+  rownames(pca.table) <- paste(pca.table.symbols, pca.table.ids, sep="_")
+  pca.table <- t(pca.table)
+  
+  res.pca <- PCA(pca.table, scale.unit = TRUE, ncp = 5, graph = FALSE)
+  var <- get_pca_var(res.pca)
+
+  # Screeplot
+  scree.plot <- fviz_eig(res.pca, addlabels = TRUE, rotate=T)
+  # Contributions of variables to PC1
+  contrib1.plot <- fviz_contrib(res.pca, choice = "var", axes = 1, top = 20, rotate=T)
+  # Contributions of variables to PC2
+  contrib2.plot <- fviz_contrib(res.pca, choice = "var", axes = 2, top = 20, rotate=T)
+  # pca plot
+  pca.plot <- fviz_pca_ind(res.pca,
+                col.ind = palette.group.names, # color by groups
+                palette = palette,
+                addEllipses = TRUE, # Concentration ellipses
+                legend.title = "Groups",
+                repel=TRUE
+               )
+
+  ggexport(plotlist = list(scree.plot, contrib1.plot, contrib2.plot), 
+         nrow = 3, ncol = 1, width=10, height=12,
+         filename = file.path(out.path, paste('pca_diag_', name.control, '_', name.treat, '.pdf', sep = ''))
+         )
+  ggexport(plotlist = list(pca.plot), 
+         nrow = 3, ncol = 1,
+         filename = file.path(out.path, paste('pca_', name.control, '_', name.treat, '.pdf', sep = ''))
+         )
+
+
 }
+
 
 # the main function
 for (ith.comparison in c(1:num.comparison)) {
